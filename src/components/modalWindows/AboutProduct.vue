0e6f61c3ca842a378333f5ecdf52c1819b7f59e7
Vladimir Kovalenko <gorsetis@yandex.ru>
2023-08-27 23:18:37 +0300

    add modalWindows

---
+++ src/components/modalWindows/AboutProduct.vue
@@ -0,0 +1,139 @@
+<script setup>
+
+import Buttons from "@/components/ui/Buttons.vue";
+
+
+const emit = defineEmits(['closeModalWindow'])
+
+const closeModalWindow = () => {
+  emit('closeModalWindow')
+}
+
+</script>
+
+<template>
+  <div class="about-product" @click.self="closeModalWindow">
+    <div class="about-product__content">
+      <div class="about-product__header">
+        <div class="about-product__title">
+          <h2>
+            <slot name="title"></slot>
+          </h2>
+        </div>
+        <Buttons btnTitle="Закрыть" @click="closeModalWindow"/>
+      </div>
+      <div class="about-product__main">
+        <div class="about-product__img">
+          <slot name="img"/>
+        </div>
+        <div class="about-product__data">
+          <p>
+            <slot name="price"/>
+          </p>
+          <p>
+            <slot name="size"/>
+          </p>
+        </div>
+
+      </div>
+
+      <div class="about-product__footer">
+        <Buttons
+            btnTitle="Назад"
+            @click="closeModalWindow"
+        />
+        <Buttons btnTitle="Добавить в корзину" :addToCart="true"/>
+
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/scss/color";
+
+.about-product {
+  box-sizing: border-box;
+  max-height: 100vh;
+  z-index: 10;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  //transform: translate(5%, 0%);
+  background-color: rgba(100, 100, 100, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__content {
+    //position: relative;
+    grid-template-rows: auto 1fr auto;
+    //grid-template-columns: 1fr 1fr;
+    display: grid;
+    grid-template-areas:
+  'header header'
+  'main main'
+  'footer footer';
+    background: $background-color;
+    border-radius: 1rem;
+    font-size: 1.23rem;
+    overflow-y: auto;
+    padding: 2rem 2rem;
+
+    width: 80vw;
+  }
+
+
+  &__header {
+    display: flex;
+    justify-content: space-between;
+    grid-area: header;
+    margin-bottom: 1rem;
+    position: relative;
+  }
+
+  &__main {
+    display: grid;
+    grid-area: main;
+    grid-template-areas:
+    'img data';
+    grid-template-columns: 3fr 1fr;
+
+
+    //position: relative;
+  }
+
+  &__data {
+    display: flex;
+    justify-content: start;
+    flex-direction: column;
+    align-items: start;
+    grid-area: data;
+    box-shadow: 0 3px 12px rgba(0, 0, 0, 1);
+    //height: max-content;
+    padding: 1rem;
+    cursor: default;
+    margin-bottom: 1rem;
+    width: max-content;
+  }
+
+  &__img {
+    display: flex;
+    flex-wrap: wrap;
+    //gap: 1px;
+    grid-area: img;
+    cursor: zoom-in;
+  }
+
+  &__footer {
+    grid-area: footer;
+    padding: 0 5rem;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
---
+++ src/components/modalWindows/LargeImg.vue
@@ -0,0 +1,67 @@
+<script setup>
+const emit = defineEmits(['close'])
+const props = defineProps({
+  image: {
+    type: Object,
+    required: true,
+    default: {}
+  }
+})
+const zoomImageCLose = () => {
+  emit('close')
+}
+</script>
+
+<template>
+  <div class="large" >
+    <div class="large__content">
+      <img :src="props.image.url" alt="props" class="large__img">
+      <span  class="zoomImageClose" @click="zoomImageCLose">X</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.large {
+  box-sizing: border-box;
+  position: absolute;
+  width: 100%;
+  height: 160vh;
+  min-height: min-content;
+  top: 0;
+  left: 0;
+  background-color: #eed0d0;
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__content {
+    display: flex;
+    width: 100%;
+    height: 100%;
+
+  }
+  &__img {
+    width: 100%;
+    object-fit: contain;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 15;
+    cursor: auto;
+  }
+}
+.zoomImageClose {
+  position: absolute;
+  top: 40px;
+  right: 160px;
+  font-size: 40px;
+  border-radius: 1rem;
+  border: 1px solid black;
+  padding: 5px 1rem;
+  background-color: #eab0b0;
+  z-index: 15;
+  cursor: pointer;
+}
+</style>
