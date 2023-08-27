6fcef04179891895957434250c89baa50fb83b6b
Vladimir Kovalenko <gorsetis@yandex.ru>
2023-08-27 23:15:25 +0300

    add layout

---
+++ src/components/layout/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+import AppNavigation from "@/components/layout/AppNavigation.vue";
+
+</script>
+
+<template>
+  <header class="header">
+    <img src="@/assets/images/logo.jpg" alt="logo" class="header__logo">
+    <AppNavigation/>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  width: 100%;
+  grid-column-gap: 3rem;
+  padding: 1rem 1rem;
+  align-items: center;
+  color: black;
+  &__logo {
+    width: 100px;
+    border-radius: 100%;
+  }
+}
+</style>
---
+++ src/components/layout/AppNavigation.vue
@@ -0,0 +1,73 @@
+<script setup>
+import {ref} from "vue";
+import basket from '@/assets/images/basket/basket.png'
+
+const navItems = ref([
+  {title: 'Главная', path: '/'},
+  {title: 'Девочки', path: '/catalog/girls'},
+  {title: 'Мальчики', path: '/catalog/boys'},
+  {title: 'Контакты', path: '/contacts'},
+  {title: 'Корзина', path: '/basket'},
+])
+</script>
+
+<template>
+  <nav class="navigation">
+    <RouterLink
+        v-for="item in navItems"
+        :to="item.path"
+        :key="item.path"
+        class="navigation__link"
+    >
+      <div v-if="item.title === 'Корзина'">
+        {{ item.title }}
+        <img :src="basket" alt="Basket">
+      </div>
+      <div v-else>
+        {{ item.title }}
+      </div>
+
+
+    </RouterLink>
+  </nav>
+
+</template>
+
+<style scoped lang="scss">
+.navigation {
+  display: flex;
+  grid-column-gap: 3rem;
+  font-size: 1.5rem;
+
+
+  &__link {
+    box-sizing: border-box;
+    padding: 5px;
+    letter-spacing: 2px;
+    text-shadow: 3px 1px 6px rgba(0, 0, 0, 1);
+
+    &:hover {
+      background-color: #ffffff;
+      border-radius: 1rem;
+      transform: translateY(-5px);
+      transition: .3s;
+
+    }
+  }
+}
+
+img {
+  width: 27px;
+}
+</style>
+
+<!--<nav class="navigation">-->
+<!--<RouterLink-->
+<!--    v-for="item in navItems"-->
+<!--    :to="item.path"-->
+<!--    :key="item.path"-->
+<!--    class="navigation__link"-->
+<!--&gt;-->
+<!--  {{ item.title }}-->
+<!--</RouterLink>-->
+<!--</nav>-->
