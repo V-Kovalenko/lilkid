12aaa6978dc86150214a94174ae4fed78e38fcd5
Vladimir Kovalenko <gorsetis@yandex.ru>
2023-08-27 23:22:36 +0300

    add pages

---
+++ src/components/pages/HomePage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {ref, computed} from "vue";
+
+import HomePageContent from "@/components/homePage/HomePageContent.vue";
+</script>
+
+<template>
+  <div class="container">
+    <HomePageContent/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-grow: 1;
+}
+</style>
